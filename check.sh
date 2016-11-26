498fe69411b1460cf19ed587d11c5969941ecf02
Alexander Georgievskiy <galeksandrp@gmail.com>
2016-11-26 04:11:35 +0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -10,7 +10,7 @@ sed -f ~/workspace/sed.xml -i "$1"
 perl -p -e 's/^replace\n//' -i "$1"
 echo '<!--' > "/tmp/$1"
 grep ERROR ~/workspace/log.txt | sed -f ~/workspace/sed2.xml >> "/tmp/$1"
-perl -p -e 's/^ nonexist\n//' -i "/tmp/$1"
+perl -p -e 's/^.* nonexist\n//' -i "/tmp/$1"
 grep 'INFO Big distance' ~/workspace/log.txt | sed -f ~/workspace/sed4.xml >> "/tmp/$1"
 echo -e '\n' >> "/tmp/$1"
 grep ' ¹' "/tmp/$1" && echo '¹ mismatch' >> "/tmp/$1"
@@ -21,8 +21,10 @@ grep ' ⁵' "/tmp/$1" && echo '⁵ expired' >> "/tmp/$1"
 grep ' ⁶' "/tmp/$1" && echo '⁶ redirect' >> "/tmp/$1"
 grep ' ⁷' "/tmp/$1" && echo '⁷ protocol error' >> "/tmp/$1"
 echo '-->' >> "/tmp/$1"
-cat "$1" >> "/tmp/$1"
-mv "/tmp/$1" "$1"
+if [ "$(cat "/tmp/$1" | sha256sum)" != "7e406daa6b81f6f133b0015c9477849d121b31d39cec8b11876f59755e8325be  -" ]; then
+  cat "$1" >> "/tmp/$1"
+  mv "/tmp/$1" "$1"
+fi
 grep "<target host=\"$DOMAIN\" />" $1 || grep "<target host=\"www.$DOMAIN\" />" $1 || exit 0
 grep "<target host=\"www.$DOMAIN\" />" $1 || sed "s&<rule from=\"^http:\" to=\"https:\" />&<target host=\"www.$DOMAIN\" />\n\n\t<rule from=\"^http://www\\\\.$ESCAPED/\"\n\t\tto=\"https://$DOMAIN/\" />\n\n\t<rule from=\"^http:\" to=\"https:\" />&" -i $1
 grep "<target host=\"$DOMAIN\" />" $1 || sed "s&<rule from=\"^http:\" to=\"https:\" />&<target host=\"$DOMAIN\" />\n\n\t<rule from=\"^http://$ESCAPED/\"\n\t\tto=\"https://www.$DOMAIN/\" />\n\n\t<rule from=\"^http:\" to=\"https:\" />&" -i $1
