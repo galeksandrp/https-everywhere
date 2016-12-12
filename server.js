faf3901a00e4e9a6ff3e184a8e60fa35f3b88c00
Alexander Georgievskiy <galeksandrp@gmail.com>
2016-12-12 00:32:25 +0000

    Add support for existing branches

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var http = require('http');
 var request = require('request');
+
+function createBranch(branch, resp) {
+  request({
+    url: 'https://' + process.env.GITHUB_NAME + ':' + process.env.GITHUB_TOKEN + '@api.github.com/repos/' + process.env.GITHUB_NAME + '/' + process.env.GITHUB_REPO + '/git/refs',
+    method: 'POST',
+    json: {
+      sha: process.env.SHA,
+      ref: 'refs/heads/' + branch
+    },
+    headers: {
+      'User-Agent': 'request'
+    }
+  }).pipe(resp);
+}
 http.createServer(function(req, resp) {
   if (req.url === '/' + process.env.PATHNAME && req.method === 'POST') {
     var jsonString = '';
@@ -10,18 +24,20 @@ http.createServer(function(req, resp) {
 
     req.on('end', function() {
       var payload = JSON.parse(jsonString);
-      if (payload.action === 'opened' || payload.action === 'reopened') {
+      if (payload.action === 'opened') {
+        createBranch(payload.issue.title + '=' + payload.issue.number, resp);
+      }
+      else if (payload.action === 'reopened') {
         request({
-          url: 'https://' + process.env.GITHUB_NAME + ':' + process.env.GITHUB_TOKEN + '@api.github.com/repos/' + process.env.GITHUB_NAME + '/' + process.env.GITHUB_REPO + '/git/refs',
-          method: 'POST',
-          json: {
-            sha: process.env.SHA,
-            ref: 'refs/heads/' + payload.issue.title + '=' + payload.issue.number
-          },
+          url: 'https://' + process.env.GITHUB_NAME + ':' + process.env.GITHUB_TOKEN + '@api.github.com/repos/' + process.env.GITHUB_NAME + '/' + process.env.GITHUB_REPO + '/git/refs/heads/' + payload.issue.title + '=' + payload.issue.number,
+          method: 'DELETE',
+          json: {},
           headers: {
             'User-Agent': 'request'
           }
-        }).pipe(resp);
+        }, function(error, response, body) {
+          createBranch(payload.issue.title + '=' + payload.issue.number, resp);
+        });
       }
       else {
         resp.end('200 OK');
