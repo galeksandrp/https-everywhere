a838690f4620504587dbfe6923dfdc05c35d818a
Alexander Georgievskiy <galeksandrp@gmail.com>
2016-12-15 00:55:44 +0000

    upd

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 var http = require('http');
 var request = require('request');
+var url = require('url');
+var crypto = require('crypto');
 
-function createBranch(branch, resp) {
-  request({
-    url: 'https://' + process.env.GITHUB_NAME + ':' + process.env.GITHUB_TOKEN + '@api.github.com/repos/' + process.env.GITHUB_NAME + '/' + process.env.GITHUB_REPO + '/git/refs',
-    method: 'POST',
-    json: {
-      sha: process.env.SHA,
-      ref: 'refs/heads/' + branch
-    },
-    headers: {
-      'User-Agent': 'request'
-    }
-  }).pipe(resp);
-}
 http.createServer(function(req, resp) {
-  if (req.url === '/' + process.env.PATHNAME && req.method === 'POST') {
+  var urlParts = url.parse(req.url);
+  if (req.url === '/' + process.env.PATHNAME && req.method === 'POST' && req.headers['X-GitHub-Event'] === 'push') {
     var jsonString = '';
 
     req.on('data', function(data) {
@@ -24,19 +14,80 @@ http.createServer(function(req, resp) {
 
     req.on('end', function() {
       var payload = JSON.parse(jsonString);
-      if (payload.action === 'opened') {
-        createBranch(payload.issue.title + '=' + payload.issue.number, resp);
+      if (payload.ref === "refs/heads/" + process.env.BRANCH) {
+        HASH = payload.after;
       }
-      else if (payload.action === 'reopened') {
+      else {
+        resp.end('200 OK');
+      }
+    });
+  }
+  else if (urlParts.pathname === '/92qhpf98qghf' && req.method === 'GET') {
+    var queries = {};
+    urlParts.query.split('&').forEach(function(query) {
+      var parts = query.split('=');
+      queries[parts[0]] = parts[1];
+    });
+    request({
+      url: 'https://github.com/login/oauth/access_token',
+      method: 'POST',
+      form: {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        code: queries.code
+      },
+      json: true,
+      headers: {
+        'User-Agent': 'request'
+      }
+    }, function(error, response, bodyAccess) {
+      if (bodyAccess.access_token) {
         request({
-          url: 'https://' + process.env.GITHUB_NAME + ':' + process.env.GITHUB_TOKEN + '@api.github.com/repos/' + process.env.GITHUB_NAME + '/' + process.env.GITHUB_REPO + '/git/refs/heads/' + payload.issue.title + '=' + payload.issue.number,
-          method: 'DELETE',
-          json: {},
+          url: 'https://api.github.com/repos/EFForg/https-everywhere/forks?access_token=' + bodyAccess.access_token,
+          method: 'POST',
+          json: true,
           headers: {
             'User-Agent': 'request'
           }
-        }, function(error, response, body) {
-          createBranch(payload.issue.title + '=' + payload.issue.number, resp);
+        }, function(error, response, bodyFork) {
+          request({
+            url: 'https://api.travis-ci.org/repos/' + process.env.GITHUB_NAME + '/httpse/key',
+            json: true
+          }, function(error, response, bodyEncrypt) {
+            var rsa = require('ursa');
+            var pem = bodyEncrypt.key.replace(/RSA PUBLIC KEY/g, 'PUBLIC KEY');
+            var publicKey = rsa.createPublicKey(pem);
+            var cipherText = publicKey.encrypt("GITHUB_TOKEN=" + bodyAccess.access_token, undefined, undefined, rsa.RSA_PKCS1_PADDING);
+            var secvar = cipherText.toString('base64');
+            //var secvar = crypto.publicEncrypt(bodyEncrypt.key, new Buffer("GITHUB_TOKEN=" + bodyAccess.access_token)).toString('base64');
+            request({
+              url: 'https://api.travis-ci.org/repo/' + process.env.GITHUB_NAME + '%2Fhttpse/requests',
+              method: 'POST',
+              json: {
+                "request": {
+                  "message": queries.state + " Override the commit message: this is an api request",
+                  "branch": "template-api",
+                  "config": {
+                    "env": {
+                      "global": [
+                        "DOMAIN=" + queries.state,
+                        "GITHUB_NAME=" + bodyFork.owner.login,
+                        "ISSUE=" + 3,
+                        {
+                          "secure": secvar
+                        }
+                      ]
+                    }
+                  }
+                }
+              },
+              headers: {
+                'User-Agent': 'request',
+                "Authorization": "token " + process.env.TRAVIS_TOKEN,
+                "Travis-API-Version": 3
+              }
+            }).pipe(resp);
+          });
         });
       }
       else {
