62b82a1224596c6e0a9c94f2853549802d4a2646
Alexander Georgievskiy <galeksandrp@gmail.com>
2016-12-20 02:39:04 +0000

    Add preload check

--- background.js
+++ background.js
@@ -1,15 +1,19 @@
-$('#fixit').click(function() {
-  $('#state').val(tldjs.getDomain($('#state').val()));
-});
-$('#forcepush').click(function() {
-  $('#url').off();
-  $('#url').submit();
-});
+var IN_PROCESS = false;
 $('#url').submit(function(event) {
-  var state = $('#state').val();
-  if (tldjs.getDomain($('#state').val()) !== state) {
-    $('body').append('<pre>' + Date() + ' possible non-domain. Fix it or push anyway</pre>');
-    $('#panel').css('display', 'block');
-    return false;
-  }
+  event.preventDefault();
+  var domain = tldjs.getDomain($('#state').val());
+  if (IN_PROCESS) return false;
+  IN_PROCESS = true;
+  $.getJSON('https://crossorigin.me/https://hstspreload.org/api/v2/status?domain=' + domain).always(function(data) {
+    IN_PROCESS = false;
+  }.done(function(data) {
+    if (data.status === 'preloaded') {
+      $('body').append('<a href="https://hstspreload.org/?domain=' + domain + '">' + domain + ' is preloaded</a>');
+    }
+    else {
+      $('#state').val(domain);
+      $('#url').off();
+      $('#url').submit();
+    }
+  }));
 });
