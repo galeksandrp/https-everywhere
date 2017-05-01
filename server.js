55dd62104cc8a998be5ae2fb14d8322bb3e030b3
Alexander Georgievskiy <galeksandrp@gmail.com>
2017-05-02 00:04:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var http = require('http');
 var request = require('request');
 
-function createBranch(branch, resp) {
+var branches = {'template-foorack2': '1d50a8f4ea414bfe298e8b8b5b7ab5a5476e2eee',
+'template-pgerber': '68a46fedc6e6b1eb7db643567f16882c9f48b1b0',
+'template-foorack': '8e6b221bf6811833b8d35d3c80c2385d0ab3ddb0'};
+
+function createBranch(branch, resp, branch) {
   request({
     url: 'https://' + process.env.GITHUB_NAME + ':' + process.env.GITHUB_TOKEN + '@api.github.com/repos/' + process.env.GITHUB_NAME + '/' + process.env.GITHUB_REPO + '/git/refs',
     method: 'POST',
     json: {
-      sha: process.env.SHA,
+      sha: typeof branch === 'undefined' ? process.env.SHA : branches[branch],
       ref: 'refs/heads/' + branch
     },
     headers: {
@@ -25,7 +29,7 @@ http.createServer(function(req, resp) {
     req.on('end', function() {
       var payload = JSON.parse(jsonString);
       if (payload.action === 'opened') {
-        createBranch(payload.issue.title + '=' + payload.issue.number, resp);
+        createBranch(payload.issue.title + '=' + payload.issue.number, resp, payload.issue.labels.length ? payload.issue.labels[0] : undefined);
       }
       else if (payload.action === 'reopened') {
         request({
@@ -36,7 +40,7 @@ http.createServer(function(req, resp) {
             'User-Agent': 'request'
           }
         }, function(error, response, body) {
-          createBranch(payload.issue.title + '=' + payload.issue.number, resp);
+          createBranch(payload.issue.title + '=' + payload.issue.number, resp, payload.issue.labels.length ? payload.issue.labels[0] : undefined);
         });
       }
       else {
